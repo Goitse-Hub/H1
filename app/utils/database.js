d027c6bb6a3149de1cd751067f4ffe09aa410b19
Goitse-Hub <121558821+Goitse-Hub@users.noreply.github.com>
2023-06-15 10:48:58 +0200

    enum

--- app/utils/database.js
+++ app/utils/database.js
@@ -1,16 +1,9 @@
-// src/utils/database.js
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+});
 
 module.exports = sequelize;
