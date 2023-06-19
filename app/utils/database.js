cbf45af69ed15ac061b8066fd7113085325cad91
Goitse-Hub <121558821+Goitse-Hub@users.noreply.github.com>
2023-06-19 07:25:28 +0200

    mon morning back

--- app/utils/database.js
+++ app/utils/database.js
@@ -4,6 +4,12 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true, // Require SSL/TLS
+      rejectUnauthorized: false, // Disable validation of SSL/TLS certificates
+    },
+  },
 });
 
 module.exports = sequelize;
